56cb71a5dfc03777a6fb1fa0267969d5308907fc
Zain <zainofficial03@gmail.com>
2021-05-26 17:19:47 +0500

    Add project files.

---
+++ SumDigit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number: ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(num % 10 + num / 10);
+
+            Console.WriteLine("\n************2nd Method******************\n");
+
+            int sum = 0;
+            while (num > 0)
+            {
+                sum = sum + num % 10;
+                num = num / 10;
+            }
+            Console.WriteLine(sum);
+
+
+            
+
+
+
+        }
+    }
+}
